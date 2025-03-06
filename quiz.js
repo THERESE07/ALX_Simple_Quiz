bb6c7b42e40d41a05e03b4ce569c649e9421c6d1
THERESE07 <niyigenatherese@gmail.com>
2025-03-07 00:23:50 +0200

    changes added

--- quiz.js
+++ quiz.js
@@ -3,9 +3,9 @@ function checkAnswer(){
     const userAnswer = document.querySelector('input[name="quiz"]:checked')?.value
     const feedback = document.getElementById('feedback');
     if(userAnswer === correctAnswer){
-        feedback.innerText("Correct! Well done");
+        feedback.textContent("Correct! Well done");
     }else{
-        feedback.innerText("That's inccorrect. Try Again!");
+        feedback.textContent("That's inccorrect. Try Again!");
     }
     
     document.getElementById("submit-answer").addEventListener("click", checkAnswer);
